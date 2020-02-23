dfbac7b826a38029b9d2c0ee58117b7d47b13b46
Craig Bailey <github@ynos.us>
2020-02-23 21:33:12 +0000

    bugfixes.. band change, cleaned up display a bit

--- vfo-bfo.ino
+++ vfo-bfo.ino
@@ -1,7 +1,7 @@
 
 /*
  VFO/BFO modified for a different display and modified for a different uC.
- forked from Ricardo Lima Caratti (PU2CLR) https://github.com/pu2clr/VFO_BFO_OLED_ARDUINO/blob/master/source/si5351_vfobfo.ino
+ forked from Ricardo Lima Caratti (PU2CLR) https://github.com/pu2clr/VFO_BFO_OLED_ARDUINO/blob/master/source/si5351_vfobfo.ino 
 */
 
 #include <si5351.h>
@@ -21,11 +21,11 @@
 #define CORRECTION_FACTOR 0 // See how to calibrate your Si5351A (0 if you do not want).
 
 #define BUTTON_STEP D7    // Control the frequency increment and decrement
-#define BUTTON_BAND D6    // Controls the band
-#define BUTTON_VFO_BFO D5 // Switch VFO to BFO
+#define BUTTON_BAND_UP D6    // Controls the band
+#define BUTTON_BAND_DN D5 // Switch VFO to BFO
 
 // BFO range for this project is 400KHz to 500KHz. The central frequency is 455KHz.
-#define MAX_BFO 0LU    // BFO maximum frequency
+#define MAX_BFO 5000000000LU    // BFO maximum frequency
 #define CENTER_BFO 0LU // BFO centeral frequency - set to zero for direct conversion
 #define MIN_BFO 0LU    // BFO minimum frequency
 
@@ -48,40 +48,42 @@ typedef struct
 // Band database. You can change the band ranges if you need.
 // The unit of frequency here is 0.01Hz (1/100 Hz). See Etherkit Library at https://github.com/etherkit/Si5351Arduino
 Band band[] = {
-    {"160m ", 17000000LLU, 200000000LLU}, // 100KHz to 1700KHz
-    {"80m tech ", 352500000LLU, 360000001LLU},   // 80m tech portion
-    {"80m  ", 350000000LLU, 400000001LLU},   // 80m ham band
+    {"AM bc", 65000000LLU, 170000000LLU},     // AM broadcast
+    {"160m ", 170000000LLU, 200000000LLU},    // 160m Ham band
+    {"80mT ", 352500000LLU, 360000001LLU},    // 80m tech portion
+    {"80m  ", 350000000LLU, 400000001LLU},    // 80m ham band
     {"SW1  ", 400000000LLU, 700000000LLU},
-    {"40m tech ", 702500000LLU, 712500000LLU},   // 40m tech portion
-    {"40m  ", 700000000LLU, 730000000LLU},   // 40m ham band
+    {"40mT ", 702500000LLU, 712500000LLU},    // 40m tech portion
+    {"40m  ", 700000000LLU, 730000000LLU},    // 40m ham band
     {"SW2  ", 730000000LLU, 1010000000LLU},   
     {"30m  ", 1010000000LLU, 1015000000LLU},  // 30m ham band
     {"SW3  ", 1015000000LLU, 1400000000LLU}, 
-    {"20m  ", 1400000000LLU, 1435000000LLU}, // 20m ham band
+    {"20m  ", 1400000000LLU, 1435000000LLU},  // 20m ham band
     {"SW4  ", 1806800000LLU, 1806800000LLU}, 
-    {"17m  ", 1806800000LLU, 1816800000LLU}, // 17m
+    {"17m  ", 1806800000LLU, 1816800000LLU},  // 17m
     {"SW5  ", 1816800000LLU, 2100000000LLU},
-    {"15m tech ", 2125000000LLU, 2120000000LLU}, // 15m tech portion
-    {"15m  ", 2100000000LLU, 2145000000LLU}, // 15m
+    {"15mT ", 2125000000LLU, 2120000000LLU},  // 15m tech portion
+    {"15m  ", 2100000000LLU, 2145000000LLU},  // 15m
     {"SW6  ", 2145000000LLU, 2498000000LLU},
-    {"12m  ", 2488000000LLU, 2499000000LLU}, // 12m
+    {"12m  ", 2488000000LLU, 2499000000LLU},  // 12m
     {"SW7  ", 2499000000LLU, 2600000000LLU},
     {"cb   ", 2600000000LLU, 2800000000LLU},
-    {"10m tech", 2800000000LLU, 2850000000LLU}, // 10m tech area
+    {"10mT ", 2800000000LLU, 2850000000LLU},  // 10m tech area
     {"10m  ", 2800000000LLU, 2970000000LLU},  // 10m general coverage
     {"VHF 1", 2970000000LLU, 5000000000LLU}, 
     {"6m   ", 5000000000LLU, 5400000000LLU},  // 6m ham band
     {"VHF2 ", 5400000000LLU, 8600000000LLU},
-    {"FM broadcast  ", 8600000000LLU, 10800000000LLU},  // Comercial FM
+    {"FM bc", 8600000000LLU, 10800000000LLU}, // Comercial FM
     {"VHF3 ", 10800000000LLU, 12000000000LLU}, 
-    {"VHF4 ", 12000000000LLU, 14400000000LLU}, // commercial/air band
-    {"2m   ", 14400000000LLU, 14800000000LLU}, // 2m ham band
-    {"VHF5 ", 14800000000LLU, 16000000000LLU}  // police/fire/pagers/etc
+    {"VHF4 ", 12000000000LLU, 14400000000LLU},// commercial/air band
+    {"2m   ", 14400000000LLU, 14800000000LLU},// 2m ham band
+    {"VHF5 ", 14800000000LLU, 16000000000LLU} // police/fire/pagers/etc
 };
 
 // Calculate the last element position (index) of the array band
 const int lastBand = (sizeof band / sizeof(Band)) - 1; // For this case will be 26.
-volatile int currentBand = 0;                          // First band. For this case, AM is the current band.
+const int firstBand = 0;                               // added for consistency
+volatile int currentBand = 1;                          // First band. For this case, AM is the current band.
 
 // Struct for step database
 typedef struct
@@ -101,20 +103,22 @@ Step step[] = {
     {"5KHz  ", 500000},
     {"10KHz ", 1000000},
     {"100KHz", 10000000},
-    {"500KHz", 50000000}}; // VFO max. increment / decrement
+    {"500KHz", 50000000},
+    {"1mHz", 100000000}
+    }; // VFO max. increment / decrement
 // Calculate the index of last position of step[] array (in this case will be 8)
 const int lastStepVFO = (sizeof step / sizeof(Step)) - 1; // index for max increment / decrement for VFO
-volatile int lastStepBFO = 3;                             // index for max. increment / decrement for BFO. In this case will be is 1KHz
-volatile long currentStep = 0;                            // it stores the current step index 
+volatile int lastStepBFO = 11;                             // index for max. increment / decrement for BFO. 
+volatile long currentStep = 4;                            // it stores the current step index 
 
 volatile boolean isFreqChanged = false;
 volatile boolean clearDisplay = false;
 
 // LW/MW is the default band
-volatile uint64_t vfoFreq = band[currentBand].minFreq; // VFO starts on AM
+volatile uint64_t vfoFreq = band[currentBand].minFreq; //
 volatile uint64_t bfoFreq = CENTER_BFO;                // 455 KHz for this project
 // VFO is the Si5351A CLK0
-// BFO is the Si5351A CLK1
+// BFO is the Si5351A CLK2
 volatile int currentClock = 0; // If 0, then VFO will be controlled else the BFO will be
 
 long volatile elapsedTimeInterrupt = millis(); // will control the minimum time to process an interrupt action
@@ -132,9 +136,9 @@ void setup()
   pinMode(ENCODER_PIN_A, INPUT);
   pinMode(ENCODER_PIN_B, INPUT);
   // Si5351 contrtolers pins
-  pinMode(BUTTON_BAND, INPUT);
+  pinMode(BUTTON_BAND_UP, INPUT);
   pinMode(BUTTON_STEP, INPUT);
-  pinMode(BUTTON_VFO_BFO, INPUT);
+  pinMode(BUTTON_BAND_DN, INPUT);
   
   // Initiating the OLED Display
   display.begin(&Adafruit128x64, I2C_ADDRESS);
@@ -159,8 +163,8 @@ void setup()
 
   // Will stop what Arduino is doing and call changeStep(), changeBand() or switchVFOBFO
   attachInterrupt(digitalPinToInterrupt(BUTTON_STEP), changeStep, RISING);      // whenever the BUTTON_STEP goes from LOW to HIGH
-  attachInterrupt(digitalPinToInterrupt(BUTTON_BAND), changeBand, RISING);      // whenever the BUTTON_BAND goes from LOW to HIGH
-  attachInterrupt(digitalPinToInterrupt(BUTTON_VFO_BFO), switchVFOBFO, RISING); // whenever the BUTTON_VFO_BFO goes from LOW to HIGH
+  attachInterrupt(digitalPinToInterrupt(BUTTON_BAND_UP), changeBandUp, RISING);      // whenever the BUTTON_BAND_UP goes from LOW to HIGH
+  attachInterrupt(digitalPinToInterrupt(BUTTON_BAND_DN), changeBandDn, RISING); // whenever the BUTTON_BAND_DN goes from LOW to HIGH
   // wait for 1/2 second and the system will be ready.
   delay(500);
 }
@@ -170,8 +174,8 @@ void setup()
 // Verificar setCursor() em https://github.com/greiman/SSD1306Ascii/issues/53
 void displayDial()
 {
-  double vfo = vfoFreq / 100000.0;
-  double bfo = bfoFreq / 100000.0;
+  double vfo = vfoFreq / 100000000.0;
+  double bfo = bfoFreq / 100000000.0;
   String mainFreq;
   String secoundFreq;
   String staticFreq;
@@ -224,11 +228,8 @@ void displayDial()
   display.println(mainFreq);
   display.set1X();
   //display.println("");
-  
-  display.print("staticFreq:");
-  display.println(staticFreq);
   display.print("band:");
-  display.println(band[currentBand].name);
+  display.print(band[currentBand].name);
   display.print("step:");
   display.println(step[currentStep].name);
   display.print("vfo/bfo adjust:");
@@ -283,18 +284,39 @@ void changeStep()
 }
 
 // Change band
-void changeBand()
+void changeBandUp()
 {
-  if ((millis() - elapsedTimeInterrupt) < MIN_ELAPSED_TIME)
-    return;                                                       // nothing to do if the time less than 11 milisecounds
-  noInterrupts();                                                 //  disable global interrupts:
-  currentBand = (currentBand < lastBand) ? (currentBand + 1) : 0; // Is the last band? If so, go to the first band (AM). Else. Else, next band.
-  vfoFreq = band[currentBand].minFreq;
-  isFreqChanged = true;
-  elapsedTimeInterrupt = millis();
-  interrupts(); // enable interrupts
+  if (!digitalRead(BUTTON_BAND_DN)) {
+    switchVFOBFO();
+  }
+  else {
+    if ((millis() - elapsedTimeInterrupt) < MIN_ELAPSED_TIME)
+      return;                                                       // nothing to do if the time less than 11 milisecounds
+    noInterrupts();                                                 //  disable global interrupts:
+    currentBand = (currentBand < lastBand) ? (currentBand + 1) : 0; // Is the last band? If so, go to the first band (AM). Else. Else, next band.
+    vfoFreq = band[currentBand].minFreq;
+    isFreqChanged = true;
+    elapsedTimeInterrupt = millis();
+    interrupts(); // enable interrupts
+  }
 }
 
+void changeBandDn()
+{
+  if (!digitalRead(BUTTON_BAND_UP)) {
+    switchVFOBFO();
+  }
+  else {
+    if ((millis() - elapsedTimeInterrupt) < MIN_ELAPSED_TIME)
+      return;                                                       // nothing to do if the time less than 11 milisecounds
+    noInterrupts();                                                 //  disable global interrupts:
+    currentBand = (currentBand > firstBand) ? (currentBand - 1) : lastBand; // Is the last band? If so, go to the first band (AM). Else. Else, next band.
+    vfoFreq = band[currentBand].minFreq;
+    isFreqChanged = true;
+    elapsedTimeInterrupt = millis();
+    interrupts(); // enable interrupts
+  }
+}
 // Switch the Encoder control from VFO to BFO and virse versa.
 void switchVFOBFO()
 {
